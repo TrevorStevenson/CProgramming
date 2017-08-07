1f8cfda96528d54b4874161a6df129700ce6dc5e
Trevor Stevenson <trevor.stevenson@duke.edu>
2017-08-06 21:59:15 -0400

    Initial Commit

---
+++ CProgramming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CProgramming
+//
+//  Created by Trevor Stevenson on 8/6/17.
+//  Copyright © 2017 TSCode. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

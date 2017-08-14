fe6000f7d9f5c4bbd3b8700824cbe690839a0f50
Trevor Stevenson <trevor.stevenson@duke.edu>
2017-08-14 01:15:17 -0400

    Added first test case

---
+++ DSA_Tests.c
@@ -0,0 +1,19 @@
+#include <CUnit/CUnit.h>
+#include "Array.h"
+
+void testArray();
+
+int main(int argc, char *argv[])
+{
+    testArray();
+    return 0;
+}
+
+void testArray()
+{
+    Array *array = createArray();
+    add(array, 3);
+    add(array, 5);
+    add(array, 6);
+    CU_ASSERT(array->size == 3);
+}
